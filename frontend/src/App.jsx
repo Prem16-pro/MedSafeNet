1b074a5d8e693c6d1d8a4361a38397dd263eedc8
Prem16-pro <premizz1652@gmail.com>
2025-01-28 12:36:54 +0530

    Jod diya firbase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,42 @@
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { app } from './firebase/firebase'
-import { AuthPages } from './pages/authPages/authPages'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { app } from "./firebase/firebase";
+import { AuthPages } from "./pages/authPages/authPages";
 import React, { useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { HomePage } from './pages/homePage/HomePage'
+import { HomePage } from "./pages/homePage/HomePage";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "./redux/authSlice";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
   const auth = getAuth();
-
+  console.log(user)
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
+        console.log(currentUser)
         // User is signed in.
-        setUser(currentUser);
+        dispatch(login(currentUser));
       } else {
         // No user is signed in.
-        setUser(null);
+        dispatch(login(null));
       }
     });
 
     // Cleanup the listener on component unmount.
     return () => unsubscribe();
   }, [auth]);
-  console.log(app)
-  return(<>
-    {user ? <HomePage/>: <AuthPages/> }
-    {/* <AuthPages/> */}
-  </>)
+  console.log(app);
+  return (
+    <>
+      {user ? <HomePage /> : <AuthPages />}
+      {/* <AuthPages/> */}
+    </>
+  );
 }
 
 export default App;
