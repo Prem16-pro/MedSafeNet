686f9c1b01569fbf0faee734691778ab2d477e20
Prem16-pro <premizz1652@gmail.com>
2025-01-28 12:46:09 +0530

    auth1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// src/App.js
+
+import { useState, useEffect } from "react";
 import { app } from "./firebase/firebase";
 import { AuthPages } from "./pages/authPages/authPages";
-import React, { useEffect } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { HomePage } from "./pages/homePage/HomePage";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "./redux/authSlice";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const user = useSelector((state) => state.auth.user);
@@ -18,23 +16,22 @@ function App() {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
-        console.log(currentUser)
-        // User is signed in.
-        dispatch(login(currentUser));
+        // Store only serializable data (uid, email)
+        dispatch(login({
+          uid: currentUser.uid,
+          email: currentUser.email,
+        }));
       } else {
-        // No user is signed in.
         dispatch(login(null));
       }
     });
 
-    // Cleanup the listener on component unmount.
     return () => unsubscribe();
-  }, [auth]);
-  console.log(app);
+  }, [auth, dispatch]);
+
   return (
     <>
       {user ? <HomePage /> : <AuthPages />}
-      {/* <AuthPages/> */}
     </>
   );
 }
--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,19 +1,32 @@
+// src/redux/authSlice.js
+
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState ={
-    user: null
+const initialState = {
+  user: null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-      login: (state, action) => {
-        state.user = action.payload;
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      if (action.payload) {
+        // Only store serializable data like uid and email
+        state.user = {
+          uid: action.payload.uid,
+          email: action.payload.email,
+        };
+      } else {
+        state.user = null;
       }
-    }
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
 });
 
-export const {login} = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
