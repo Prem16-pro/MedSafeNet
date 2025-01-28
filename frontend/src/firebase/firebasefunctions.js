54a8ab1355ab692b56293ad74875b6295327282d
Goldenbook27 <may.pranjali.naik@gmail.com>
2025-01-28 12:57:23 +0530

    intitalized user

--- frontend/src/firebase/firebasefunctions.js
+++ frontend/src/firebase/firebasefunctions.js
@@ -1,43 +1,78 @@
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+} from "firebase/firestore";
+
+const db = getFirestore();
 const auth = getAuth();
 
-export const createUser = (email,password)=>{
-    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    console.log("User created successfully")
-    console.log(user)
-    // ...
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
-    // ..
-  });
-}
-export const logInUser = (email,password)=>{
-    signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user)
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
-  });
-}
-export const logOut = ()=>{
-    signOut(auth).then(() => {
-        // Sign-out successful.
-        console.log("Log out successfull")
-      }).catch((error) => {
-        // An error happened.
-        console.log(error)
+export const createUser = (email, password, username, role) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      console.log("User created successfully");
+      console.log(user);
+      console.log(user.uid);
+      intializeUser(email, password, username, role, user.uid);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ..
+    });
+};
 
-      });
-}
+export const logInUser = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+};
+
+export const logOut = () => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      console.log("Log out successfull");
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+};
+
+const intializeUser = async (email, password, username, role, uid) => {
+  try {
+    const docRef = await setDoc(doc(db, "users", uid), {
+      email: email,
+      password: password,
+      username: username,
+      role: role,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
--- frontend/src/pages/authPages/AuthPages.jsx
+++ frontend/src/pages/authPages/AuthPages.jsx
@@ -1,56 +1,104 @@
-import { useState } from "react"
-import { createUser, logInUser } from "../../firebase/firebasefunctions"
+import { useState } from "react";
+import { createUser, logInUser } from "../../firebase/firebasefunctions";
 
 export const AuthPages = () => {
-    const [loginPage, setLoginPage] = useState(false)
-    return(<>
-    {loginPage ? <LoginPage setLoginPage={setLoginPage} /> : <SignUpPage setLoginPage={setLoginPage} />}
-    </>)
-}
+  const [loginPage, setLoginPage] = useState(false);
+  return (
+    <>
+      {loginPage ? (
+        <LoginPage setLoginPage={setLoginPage} />
+      ) : (
+        <SignUpPage setLoginPage={setLoginPage} />
+      )}
+    </>
+  );
+};
 
-const LoginPage = ({setLoginPage})=>{
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const handleLogIn = ()=>{
-        console.log(email)
-        console.log(password)
-        logInUser(email,password)
-    }
-    return(
-        <>
-        <div className="container">
+const LoginPage = ({ setLoginPage }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogIn = () => {
+    console.log(email);
+    console.log(password);
+    logInUser(email, password);
+  };
+  return (
+    <>
+      <div className="container">
         <div>Login Page</div>
-        <input placeholder="Enter email" name="email" onChange={(e)=>{setEmail(e.target.value)}}></input>
-        <input placeholder="Enter password" name="password" onChange={(e)=>{setPassword(e.target.value)}}></input>
+        <input
+          placeholder="Enter email"
+          name="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <input
+          placeholder="Enter password"
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
         <button onClick={handleLogIn}>Log In</button>
         <div>Don't have an account? </div>
-        <button onClick={()=>{setLoginPage(false)}}>Sign Up</button>
-        </div>
-        </>
-    )
-}
+        <button
+          onClick={() => {
+            setLoginPage(false);
+          }}
+        >
+          Sign Up
+        </button>
+      </div>
+    </>
+  );
+};
 
-const SignUpPage = ({setLoginPage}) => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const handleSignUp = ()=>{
-        console.log(email)
-        console.log(password)
-        createUser(email,password)
-    }
-    return (
+const SignUpPage = ({ setLoginPage }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("doctor");
+  const [username, setUserName] = useState("");
+  const handleSignUp = () => {
+    console.log(email);
+    console.log(password);
+    createUser(email, password, username, role);
+  };
+  return (
     <>
-        <div className="container">
+      <div className="container">
         <div>Sign Up</div>
-        <input placeholder="Enter email" name="email" onChange={(e)=>{setEmail(e.target.value)}}></input>
-        <input placeholder="Enter password" name="password" onChange={(e)=>{setPassword(e.target.value)}}></input>
+        <input
+          placeholder="Enter username"
+          name="username"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        ></input>
+        <input
+          placeholder="Enter email"
+          name="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <input
+          placeholder="Enter password"
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
         <button onClick={handleSignUp}>Sign Up</button>
         <div>Already have an account? </div>
-            <button onClick={()=>{
-                setLoginPage(true)
-            }}>Log In</button>
-        
-        </div>
-        </>
-    )
-}
+        <button
+          onClick={() => {
+            setLoginPage(true);
+          }}
+        >
+          Log In
+        </button>
+      </div>
+    </>
+  );
+};
